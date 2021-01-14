e0794406d4eca6243ad440443521dbcdcb752080
DoHaiNam <dohainam301091@gmail.com>
2021-01-14 16:57:06 +0700

    separate api script

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import { createTodo, getAllTodos, editTodo, getTodoDetail } from './apis/todosApis'
+
 function onEditTodo (todo) {
     todoDetailWrapper.innerHTML = `
       <div>
@@ -8,54 +10,6 @@ function onEditTodo (todo) {
         <span>end_at: ${todo.end_at}</span>
         <span>avatar: ${todo.avatar}</span>
       </div>`
-}
-
-const getAllTodos = () => {
-    return new Promise(res => {
-        fetch('https://5ff6f759e7164b0017e19f4b.mockapi.io/todo/v1/todo/')
-            .then((response) => response.json())
-            .then((json) => {
-                res(json)
-            });
-    })
-}
-
-const getTodoDetail = (todoId) => {
-    return new Promise(res => {
-        fetch(`https://5ff6f759e7164b0017e19f4b.mockapi.io/todo/v1/todo/${todoId}`)
-            .then((response) => response.json())
-            .then(json => res(json))
-    })
-}
-
-const editTodo = (data) => {
-    return new Promise(res => {
-        fetch('https://5ff6f759e7164b0017e19f4b.mockapi.io/todo/v1/todo/', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-            .then((response) => response.json())
-            .then(json => res(json))
-            .then(() => getAllTodos())
-    })
-}
-
-const createTodo = (data) => {
-    return new Promise(res => {
-        fetch('https://5ff6f759e7164b0017e19f4b.mockapi.io/todo/v1/todo/', {
-          method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-            .then((response) => response.json())
-            .then(json => res(json))
-            .then(() => getAllTodos())
-    })
 }
 
 const wrapper = document.getElementById('app')
